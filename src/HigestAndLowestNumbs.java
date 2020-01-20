799d2ce4e15251866161c15e094d19f9aaa708fb
Siva <sivav8g@gmail.com>
2020-01-20 23:05:29 +0530

    first Commit

---
+++ src/HigestAndLowestNumbs.java
@@ -0,0 +1,19 @@
+
+public class HigestAndLowestNumbs {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 5, 8, 6, 4, 2, 7 };
+		int hig = 0, low = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > hig)
+				hig = arr[i];
+			else if (arr[i] < low)
+				low = arr[i];
+
+		}
+		System.out.println(hig + "        " + low);
+
+	}
+
+}
