94b9768a00a36922a4c9e6cf71679581301b0e3a
Siva <sivav8g@gmail.com>
2020-02-17 09:46:12 +0530

    Add Condtion in file

--- src/HigestAndLowestNumbs.java
+++ src/HigestAndLowestNumbs.java
@@ -6,6 +6,7 @@ public class HigestAndLowestNumbs {
 		int[] arr = { 5, 8, 6, 4, 2, 7 };
 		int hig = 0, low = 0;
 		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Add comments in Git");
 			if (arr[i] > hig)
 				hig = arr[i];
 			else if (arr[i] < low)
